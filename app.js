f69c709c04defb7b8184777f453ce06f5edfcf26
Laura Wu <laura.jh.wu@gmail.com>
2021-01-27 23:28:22 +0800

    updated index, show, and search bar with the addition of mongodb

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const exphbr = require('express-handlebars');
 const mongoose = require('mongoose');
 const port = 3000;
 
+const Restaurant = require('./models/restaurant');
+
 //connect to mongodb and store connection status
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection
@@ -25,29 +27,38 @@ app.listen(port, () => {
     console.log(`Express is listening to localhost:${port}`);
 })
 
-//DISPLAY CONTENT AND SET FUNCTIONS///////
+//DISPLAY CONTENT AND SET SEARCH BAR///////
 //set route for index page (home page)
 app.get('/', (req, res) => {
-    //upload restaurant data to page using handlebars' {{#each}}
-    res.render('index', { restaurants: restaurantList.results });
+    //get all restaurant data and upload to index page
+    return Restaurant.find()
+        .lean()
+        .then(restaurants => res.render('index', { restaurants }))
+        .catch(error => console.log(error))
 })
 
 //set route for show page (info page)
 app.get('/restaurants/:id', (req, res) => {
     //get the restaurant that matches the restaurant id clicked by the user
-    const restaurant = restaurantList.results.find(restaurant => {
-        return restaurant.id.toString() === req.params.id;
-    })
-    res.render('show', { restaurant: restaurant });
+    const id = req.params.id
+    return Restaurant.findById(id)
+        .lean()
+        .then(restaurant => res.render('show', { restaurant }))
+        .catch(error => console.log(error))
 })
 
 //set route for search bar
 app.get('/search', (req, res) => {
     const keyword = req.query.keyword;
-    const restaurants = restaurantList.results.filter(restaurant => {
-        return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase())
-            || restaurant.name_en.toLowerCase().includes(keyword.trim().toLowerCase());
-    })
-    //create keyword variable to keep users' query
-    res.render('index', { restaurants: restaurants, keyword: keyword });
+    //get all restaurant data to see if keyword matches
+    return Restaurant.find()
+        .lean()
+        .then(restaurants => {
+            const restaurantFilter = restaurants.filter(restaurant => {
+                return restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase())
+                    || restaurant.name_en.toLowerCase().includes(keyword.trim().toLowerCase());
+            })
+            //get matches and create keyword variable to keep users' query
+            res.render('index', { restaurants: restaurantFilter, keyword });
+        })
 })
