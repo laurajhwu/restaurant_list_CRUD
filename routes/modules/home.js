3ac3622c22ebe1f8f1e5c3630241bef3f78fc3d1
Laura <laura.jh.wu@gmail.com>
2021-02-01 23:35:18 +0800

    refactor: made minor adjustments to home page route

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,13 +4,11 @@ const Restaurant = require('../../models/restaurant');
 
 //set route for index page (home page)
 router.get('/', (req, res) => {
-    //set default value for sort dropdown
-    const currentSelection = '請選則排列';
     //get all restaurant data and upload to index page
     return Restaurant.find()
         .lean()
         .sort({ _id: 'asc' })
-        .then(restaurants => res.render('index', { restaurants, currentSelection }))
+        .then(restaurants => res.render('index', { restaurants }))
         .catch(error => console.log(error))
 })
 
